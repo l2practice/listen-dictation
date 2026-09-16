75f96993f2b6d8d2950566ecd70213ae69fc4b2f
l2practice <edragoncloud@gmail.com>
2026-09-16 08:51:09 +0700

    Update ld-common.js

--- ld-common.js
+++ ld-common.js
@@ -10,7 +10,7 @@
 (function (global) {
   'use strict';
 
-  var GAS = 'https://script.google.com/macros/s/AKfycbzByaIjPqA1nKLEFtyGIsDTUiGaE4HA0rb0TEiA5oXj6LqANjc4Wa2EZHO3gOyNZXyH/exec';
+  var GAS = 'https://script.google.com/macros/s/AKfycbyadq7DEYYcTNKILHotdXw7cCElBwggj4JGHJ3JD6tM07agn1CQq6aSklIwii5G0iiQ/exec';
 
   var LD = {
     GAS: GAS,
